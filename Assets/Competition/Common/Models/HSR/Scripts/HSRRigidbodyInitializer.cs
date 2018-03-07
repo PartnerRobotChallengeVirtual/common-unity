83c918245b58dd4889faa271804decb43a472620
hiroki-yamada <yamada4tec@gmail.com>
2018-03-07 12:45:15 +0900

    Update: add center of mass of HSR

---
+++ Assets/Competition/Common/Models/HSR/Scripts/HSRRigidbodyInitializer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HSRRigidbodyInitializer : MonoBehaviour
+{
+	void Awake()
+	{
+		Rigidbody bodyRigidbody = GetComponent<Rigidbody>();
+
+		bodyRigidbody.centerOfMass = new Vector3(0.0f, 0.0f, 0.1f);
+	}
+}
